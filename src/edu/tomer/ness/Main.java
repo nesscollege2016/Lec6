ce37e4d605ac44448b30fa3470b24b123a2ba895
nesscollege2016 <nesscollege2016@gmail.com>
2016-02-22 21:28:57 +0200

    shorthand if statements

--- src/edu/tomer/ness/Main.java
+++ src/edu/tomer/ness/Main.java
@@ -1,15 +1,23 @@
 package edu.tomer.ness;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        double sum = 0;
 
-        for (int rows = 0; rows < 20; rows++) {
-            for (int col = 0; col < 10; col++) {
-                System.out.print(rows % 2 == 0? "*" : "^");
+        int counter = 0;
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Enter a number");
+            int num = scan.nextInt();
+            System.out.println(num < 100 && num > 0 ? "In Range": "Not in range");
+            if (num < 100 && num > 0){
+                sum = sum + num;
+                counter++;
             }
-            System.out.println();
         }
-
+        System.out.println(sum / counter);
     }
 }
